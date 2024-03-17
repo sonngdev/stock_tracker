0a86a399507a3b7c7541986d0987f1c762fd043d
Sơn Nguyễn <son@sonng.dev>
2024-03-17 01:06:55 +0000

    feat: create fetched data model

--- stocks/models.py
+++ stocks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class FetchedData(models.Model):
+    symbol = models.CharField(max_length=10)
+    cash_flow = models.BigIntegerField()
+    total_debt = models.BigIntegerField()
+    cash = models.BigIntegerField()
+    growth_1_3 = models.FloatField()
+    growth_4_10 = models.FloatField()
+    beta = models.FloatField()
+    shares_outstanding = models.BigIntegerField()
+    stated_at = models.DateField("date of the financial statements")
+
+    def __str__(self) -> str:
+        return f"{self.symbol} ({self.stated_at})"
