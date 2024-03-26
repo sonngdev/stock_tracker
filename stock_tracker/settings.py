fc8e6cf93115e0d34ef28c0e8a61d159d83bddf9
Sơn Nguyễn <son@sonng.dev>
2024-03-26 21:30:52 +0000

    chore: set up postgresql db for prod

--- stock_tracker/settings.py
+++ stock_tracker/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +77,18 @@ WSGI_APPLICATION = "stock_tracker.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+dev_db = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+prod_db = {"default": dj_database_url.config(conn_max_age=600)}
+
+env = os.getenv("ENV", "Development")
+
+DATABASES = dev_db if env == "Development" else prod_db
 
 
 # Password validation
